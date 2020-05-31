69bb87a51b3e1d6c5fc000dd7c65b40ccb5c4e38
Nick Packham <46549847+NickPackham@users.noreply.github.com>
2020-05-31 15:09:45 -0700

    Create config.lua

---
+++ lua/voy_intercom_config/config.lua
@@ -0,0 +1,7 @@
+VOY_Config_Intercom = {}
+
+VOY_Config_Intercom_StartSound = "addoncontent/intercom/intercomstart.mp3"
+
+VOY_Config_Intercom_EndSound = "addoncontent/intercom/intercomend.mp3"
+
+VOY_Config_Intercom_Model = "models/kingpommes/starwars/misc/bridge_console4.mdl"
